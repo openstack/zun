5fd2a77893bc6311ceb47751cb40dff96eabc73f
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-05-16 17:41:10 +0900

    Cleanup unnecessary pecan files
    Change-Id: I328c85769b070cc8753ce9eba523b2555496734f
    Partially-Implements: blueprint pecan-project-higgins

--- higgins/api/config.py
+++ higgins/api/config.py
@@ -12,59 +12,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-# Server Specific Configurations
-server = {
-    'port': '8080',
-    'host': '0.0.0.0'
-}
-
 # Pecan Application Configurations
 app = {
     'root': 'higgins.controllers.root.RootController',
     'modules': ['higgins'],
-    'static_root': '%(confdir)s/public',
-    'template_path': '%(confdir)s/higgins/templates',
     'debug': True,
-    'errors': {
-        404: '/error/404',
-        '__force_dict__': True
-    }
 }
-
-logging = {
-    'root': {'level': 'INFO', 'handlers': ['console']},
-    'loggers': {
-        'higgins': {'level': 'DEBUG', 'handlers': ['console'],
-                    'propagate': False},
-        'pecan': {'level': 'DEBUG', 'handlers': ['console'],
-                  'propagate': False},
-        'py.warnings': {'handlers': ['console']},
-        '__force_dict__': True
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'color'
-        }
-    },
-    'formatters': {
-        'simple': {
-            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
-                       '[%(threadName)s] %(message)s')
-        },
-        'color': {
-            '()': 'pecan.log.ColorFormatter',
-            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
-                       '[%(threadName)s] %(message)s'),
-            '__force_dict__': True
-        }
-    }
-}
-
-# Custom Configurations must be in Python dictionary format::
-#
-# foo = {'bar':'baz'}
-#
-# All configurations are accessible at::
-# pecan.conf
