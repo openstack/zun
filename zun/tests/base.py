9cb66f6168d6a8064aeee4030704bca5fe4a9d7b
Madhuri Kumari <madhuri.kumari@intel.com>
2016-06-10 15:26:36 +0530

    Add missing parts for Zun test
    This patch adds following:
    * Base Test for Zun
    * Functional directory for functional tests
    * Base object
    
    Change-Id: I21d934b81411860af78813d58d6406b59e411583

--- zun/tests/base.py
+++ zun/tests/base.py
@@ -13,9 +13,18 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import copy
+import os
+
+import mock
 from oslo_config import cfg
 from oslo_log import log
 from oslotest import base
+import pecan
+import testscenarios
+
+from zun.common import context as zun_context
+from zun.objects import base as objects_base
 
 from zun.tests import conf_fixture
 
@@ -28,9 +37,84 @@ except cfg.ArgsAlreadyParsedError:
 CONF.set_override('use_stderr', False)
 
 
-class BaseTestCase(base.BaseTestCase):
-    """Test case base class for all unit tests."""
+class BaseTestCase(testscenarios.WithScenarios, base.BaseTestCase):
+    """Test base class."""
 
     def setUp(self):
         super(BaseTestCase, self).setUp()
+        self.addCleanup(cfg.CONF.reset)
+
+
+class TestCase(base.BaseTestCase):
+    """Test case base class for all unit tests."""
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        token_info = {
+            'token': {
+                'project': {
+                    'id': 'fake_project'
+                },
+                'user': {
+                    'id': 'fake_user'
+                }
+            }
+        }
+        self.context = zun_context.RequestContext(
+            auth_token_info=token_info,
+            project_id='fake_project',
+            user_id='fake_user')
+
+        def make_context(*args, **kwargs):
+            # If context hasn't been constructed with token_info
+            if not kwargs.get('auth_token_info'):
+                kwargs['auth_token_info'] = copy.deepcopy(token_info)
+            if not kwargs.get('project_id'):
+                kwargs['project_id'] = 'fake_project'
+            if not kwargs.get('user_id'):
+                kwargs['user_id'] = 'fake_user'
+
+            context = zun_context.RequestContext(*args, **kwargs)
+            return zun_context.RequestContext.from_dict(context.to_dict())
+
+        p = mock.patch.object(zun_context, 'make_context',
+                              side_effect=make_context)
+        self.mock_make_context = p.start()
+        self.addCleanup(p.stop)
+
         self.useFixture(conf_fixture.ConfFixture())
+
+        self._base_test_obj_backup = copy.copy(
+            objects_base.HigginsObjectRegistry._registry._obj_classes)
+        self.addCleanup(self._restore_obj_registry)
+
+        def reset_pecan():
+            pecan.set_config({}, overwrite=True)
+
+        self.addCleanup(reset_pecan)
+
+    def _restore_obj_registry(self):
+        objects_base.HigginsObjectRegistry._registry._obj_classes \
+            = self._base_test_obj_backup
+
+    def config(self, **kw):
+        """Override config options for a test."""
+        group = kw.pop('group', None)
+        for k, v in kw.items():
+            CONF.set_override(k, v, group)
+
+    def get_path(self, project_file=None):
+        """Get the absolute path to a file. Used for testing the API.
+
+        :param project_file: File whose path to return. Default: None.
+        :returns: path to the specified file, or path to project root.
+        """
+        root = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            '..',
+                                            '..',
+                                            )
+                               )
+        if project_file:
+            return os.path.join(root, project_file)
+        else:
+            return root
