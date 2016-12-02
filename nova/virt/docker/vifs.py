24a96f5d599cab62850793e07b8ff20f061adaa2
Hongbin Lu <hongbin.lu@huawei.com>
2016-12-02 16:29:26 -0600

    Switch to os-vif library
    Replace vif plugging/unplugging codes with os-vif calls. os-vif
    is a library for plugging and unplugging virtual interfaces in
    OpenStack. Currently, it supports two vif types: OVS and Linux
    bridge. Leverage this library allows us to offload some networking
    codes and reduce the maintenance burden.
    
    Partial-Implements: blueprint switch-to-os-vif
    Change-Id: I143ec12c54dd7af2a8c30860222ca1f9c513c7bf

--- nova/virt/docker/vifs.py
+++ nova/virt/docker/vifs.py
@@ -14,7 +14,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import random
 
+import os_vif
+from os_vif import exception as osv_exception
 from oslo_concurrency import processutils
 from oslo_log import log as logging
 
@@ -23,10 +26,10 @@ from nova.i18n import _
 from nova.network import linux_net
 from nova.network import manager
 from nova.network import model as network_model
+from nova.network import os_vif_util
 from nova import utils
 from nova.virt.docker import network
 from oslo_config import cfg
-import random
 
 # We need config opts from manager, but pep8 complains, this silences it.
 assert manager
@@ -49,24 +52,33 @@ class DockerGenericVIFDriver(object):
                    'vif': vif})
 
         if vif_type is None:
-            raise exception.NovaException(
+            raise exception.VirtualInterfacePlugException(
                 _("vif_type parameter must be present "
                   "for this vif_driver implementation"))
 
-        if vif_type == network_model.VIF_TYPE_BRIDGE:
-            self.plug_bridge(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_OVS:
-            if self.ovs_hybrid_required(vif):
-                self.plug_ovs_hybrid(instance, vif)
-            else:
-                self.plug_ovs(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_MIDONET:
-            self.plug_midonet(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_IOVISOR:
-            self.plug_iovisor(instance, vif)
-        else:
-            raise exception.NovaException(
-                _("Unexpected vif_type=%s") % vif_type)
+        # Try os-vif codepath first
+        vif_obj = os_vif_util.nova_to_osvif_vif(vif)
+        if vif_obj is not None:
+            self._plug_os_vif(instance, vif_obj)
+            return
+
+        # Legacy non-os-vif codepath
+        func = getattr(self, 'plug_%s' % vif_type, None)
+        if not func:
+            raise exception.VirtualInterfacePlugException(
+                _("Plug vif failed because of unexpected "
+                  "vif_type=%s") % vif_type)
+        func(instance, vif)
+
+    def _plug_os_vif(self, instance, vif):
+        instance_info = os_vif_util.nova_to_osvif_instance(instance)
+
+        try:
+            os_vif.plug(vif, instance_info)
+        except osv_exception.ExceptionBase as ex:
+            msg = (_("Failure running os_vif plugin plug method: %(ex)s")
+                   % {'ex': ex})
+            raise exception.NovaException(msg)
 
     def plug_iovisor(self, instance, vif):
         """Plug docker vif into IOvisor
@@ -104,31 +116,6 @@ class DockerGenericVIFDriver(object):
             msg = _('Failed to setup the network, rolling back')
             undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
 
-    def plug_ovs(self, instance, vif):
-        if_local_name = 'tap%s' % vif['id'][:11]
-        if_remote_name = 'ns%s' % vif['id'][:11]
-        bridge = vif['network']['bridge']
-
-        # Device already exists so return.
-        if linux_net.device_exists(if_local_name):
-            return
-        undo_mgr = utils.UndoManager()
-
-        try:
-            utils.execute('ip', 'link', 'add', 'name', if_local_name, 'type',
-                          'veth', 'peer', 'name', if_remote_name,
-                          run_as_root=True)
-            linux_net.create_ovs_vif_port(bridge, if_local_name,
-                                          network.get_ovs_interfaceid(vif),
-                                          vif['address'],
-                                          instance['uuid'])
-            utils.execute('ip', 'link', 'set', if_local_name, 'up',
-                          run_as_root=True)
-        except Exception:
-            LOG.exception("Failed to configure network")
-            msg = _('Failed to setup the network, rolling back')
-            undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
-
     def plug_midonet(self, instance, vif):
         """Plug into MidoNet's network port
 
@@ -136,7 +123,7 @@ class DockerGenericVIFDriver(object):
         """
         if_local_name = 'tap%s' % vif['id'][:11]
         if_remote_name = 'ns%s' % vif['id'][:11]
-        port_id = network.get_ovs_interfaceid(vif)
+        port_id = vif['id']
 
         # Device already exists so return.
         if linux_net.device_exists(if_local_name):
@@ -158,72 +145,6 @@ class DockerGenericVIFDriver(object):
             msg = _('Failed to setup the network, rolling back')
             undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
 
-    def plug_ovs_hybrid(self, instance, vif):
-        """Plug using hybrid strategy
-
-        Create a per-VIF linux bridge, then link that bridge to the OVS
-        integration bridge via a veth device, setting up the other end
-        of the veth device just like a normal OVS port.  Then boot the
-        VIF on the linux bridge. and connect the tap port to linux bridge
-        """
-
-        iface_id = self.get_ovs_interfaceid(vif)
-        br_name = self.get_br_name(vif['id'])
-        v1_name, v2_name = self.get_veth_pair_names(vif['id'])
-
-        undo_mgr = utils.UndoManager()
-
-        try:
-            if not linux_net.device_exists(br_name):
-                utils.execute('brctl', 'addbr', br_name, run_as_root=True)
-                # Incase of failure undo the Steps
-                # Deleting/Undoing the interface will delete all
-                # associated resources
-                undo_mgr.undo_with(lambda: utils.execute(
-                    'brctl', 'delbr', br_name, run_as_root=True))
-                # LOG.exception('Throw Test exception with bridgename %s'
-                # % br_name)
-
-                utils.execute('brctl', 'setfd', br_name, 0, run_as_root=True)
-                utils.execute('brctl', 'stp', br_name, 'off', run_as_root=True)
-                utils.execute('tee',
-                              ('/sys/class/net/%s/bridge/multicast_snooping' %
-                               br_name),
-                              process_input='0',
-                              run_as_root=True,
-                              check_exit_code=[0, 1])
-
-            if not linux_net.device_exists(v2_name):
-                linux_net._create_veth_pair(v1_name, v2_name)
-                undo_mgr.undo_with(lambda: utils.execute(
-                    'ip', 'link', 'delete', v1_name, run_as_root=True))
-
-                utils.execute('ip', 'link', 'set', br_name, 'up',
-                              run_as_root=True)
-                undo_mgr.undo_with(lambda: utils.execute('ip', 'link', 'set',
-                                                         br_name, 'down',
-                                                         run_as_root=True))
-
-                # Deleting/Undoing the interface will delete all
-                # associated resources (remove from the bridge, its
-                # pair, etc...)
-                utils.execute('brctl', 'addif', br_name, v1_name,
-                              run_as_root=True)
-
-                linux_net.create_ovs_vif_port(self.get_bridge_name(vif),
-                                              v2_name,
-                                              iface_id, vif['address'],
-                                              instance['uuid'])
-                undo_mgr.undo_with(
-                    lambda: utils.execute('ovs-vsctl', 'del-port',
-                                          self.get_bridge_name(vif),
-                                          v2_name, run_as_root=True))
-        except Exception:
-            msg = "Failed to configure Network." \
-                " Rolling back the network interfaces %s %s %s " % (
-                    br_name, v1_name, v2_name)
-            undo_mgr.rollback_and_reraise(msg=msg, instance=instance)
-
     # We are creating our own mac's now because the linux bridge interface
     # takes on the lowest mac that is assigned to it.  By using FE range
     # mac's we prevent the interruption and possible loss of networking
@@ -235,32 +156,6 @@ class DockerGenericVIFDriver(object):
                random.randint(0x00, 0xff),
                random.randint(0x00, 0xff)]
         return ':'.join(map(lambda x: "%02x" % x, mac))
-
-    def plug_bridge(self, instance, vif):
-        bridge = vif['network']['bridge']
-        gateway = network.find_gateway(instance, vif['network'])
-
-        net = vif['network']
-        if net.get_meta('should_create_vlan', False):
-            vlan = net.get_meta('vlan'),
-            iface = (CONF.vlan_interface or
-                     vif['network'].get_meta('bridge_interface'))
-            linux_net.LinuxBridgeInterfaceDriver.ensure_vlan_bridge(
-                vlan,
-                bridge,
-                iface,
-                net_attrs=vif,
-                mtu=vif.get('mtu'))
-            iface = 'vlan%s' % vlan
-        else:
-            iface = (CONF.flat_interface or
-                     vif['network'].get_meta('bridge_interface'))
-            LOG.debug('Ensuring bridge for %s - %s' % (iface, bridge))
-            linux_net.LinuxBridgeInterfaceDriver.ensure_bridge(
-                bridge,
-                iface,
-                net_attrs=vif,
-                gateway=gateway)
 
     def unplug(self, instance, vif):
         vif_type = vif['type']
@@ -275,20 +170,28 @@ class DockerGenericVIFDriver(object):
                 _("vif_type parameter must be present "
                   "for this vif_driver implementation"))
 
-        if vif_type == network_model.VIF_TYPE_BRIDGE:
-            self.unplug_bridge(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_OVS:
-            if self.ovs_hybrid_required(vif):
-                self.unplug_ovs_hybrid(instance, vif)
-            else:
-                self.unplug_ovs(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_MIDONET:
-            self.unplug_midonet(instance, vif)
-        elif vif_type == network_model.VIF_TYPE_IOVISOR:
-            self.unplug_iovisor(instance, vif)
-        else:
+        # Try os-vif codepath first
+        vif_obj = os_vif_util.nova_to_osvif_vif(vif)
+        if vif_obj is not None:
+            self._unplug_os_vif(instance, vif_obj)
+            return
+
+        # Legacy non-os-vif codepath
+        func = getattr(self, 'unplug_%s' % vif_type, None)
+        if not func:
             raise exception.NovaException(
                 _("Unexpected vif_type=%s") % vif_type)
+        func(instance, vif)
+
+    def _unplug_os_vif(self, instance, vif):
+        instance_info = os_vif_util.nova_to_osvif_instance(instance)
+
+        try:
+            os_vif.unplug(vif, instance_info)
+        except osv_exception.ExceptionBase as ex:
+            msg = (_("Failure running os_vif plugin unplug method: %(ex)s")
+                   % {'ex': ex})
+            raise exception.NovaException(msg)
 
     def unplug_iovisor(self, instance, vif):
         """Unplug vif from IOvisor
@@ -308,14 +211,6 @@ class DockerGenericVIFDriver(object):
         except processutils.ProcessExecutionError:
             LOG.exception(_("Failed while unplugging vif"), instance=instance)
 
-    def unplug_ovs(self, instance, vif):
-        """Unplug the VIF by deleting the port from the bridge."""
-        try:
-            linux_net.delete_ovs_vif_port(vif['network']['bridge'],
-                                          vif['devname'])
-        except processutils.ProcessExecutionError:
-            LOG.exception(_("Failed while unplugging vif"), instance=instance)
-
     def unplug_midonet(self, instance, vif):
         """Unplug into MidoNet's network port
 
@@ -323,38 +218,9 @@ class DockerGenericVIFDriver(object):
         """
         try:
             utils.execute('mm-ctl', '--unbind-port',
-                          network.get_ovs_interfaceid(vif), run_as_root=True)
+                          vif['id'], run_as_root=True)
         except processutils.ProcessExecutionError:
             LOG.exception(_("Failed while unplugging vif"), instance=instance)
-
-    def unplug_ovs_hybrid(self, instance, vif):
-        """UnPlug using hybrid strategy
-
-        Unhook port from OVS, unhook port from bridge, delete
-        bridge, and delete both veth devices.
-        """
-        try:
-            br_name = self.get_br_name(vif['id'])
-            v1_name, v2_name = self.get_veth_pair_names(vif['id'])
-
-            if linux_net.device_exists(br_name):
-                utils.execute('brctl', 'delif', br_name, v1_name,
-                              run_as_root=True)
-                utils.execute('ip', 'link', 'set', br_name, 'down',
-                              run_as_root=True)
-                utils.execute('brctl', 'delbr', br_name,
-                              run_as_root=True)
-
-            linux_net.delete_ovs_vif_port(self.get_bridge_name(vif),
-                                          v2_name)
-        except processutils.ProcessExecutionError:
-            LOG.exception(_("Failed while unplugging vif"), instance=instance)
-
-    def unplug_bridge(self, instance, vif):
-        # NOTE(arosen): nothing has to be done in the linuxbridge case
-        # as when the veth is deleted it automatically is removed from
-        # the bridge.
-        pass
 
     def _create_veth_pair(self, if_local_name, if_remote_name, bridge):
         undo_mgr = utils.UndoManager()
@@ -432,18 +298,8 @@ class DockerGenericVIFDriver(object):
         except Exception:
             LOG.exception("Failed to attach vif")
 
-    def get_bridge_name(self, vif):
-        return vif['network']['bridge']
-
-    def get_ovs_interfaceid(self, vif):
-        return vif.get('ovs_interfaceid') or vif['id']
-
     def get_br_name(self, iface_id):
         return ("qbr" + iface_id)[:network_model.NIC_NAME_LEN]
-
-    def get_veth_pair_names(self, iface_id):
-        return (("qvb%s" % iface_id)[:network_model.NIC_NAME_LEN],
-                ("qvo%s" % iface_id)[:network_model.NIC_NAME_LEN])
 
     def ovs_hybrid_required(self, vif):
         ovs_hybrid_required = self.get_firewall_required(vif) or \
