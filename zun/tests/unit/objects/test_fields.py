c0cebba170b8e3ea5e62e335536cf974bbbf08ec
Hongbin Lu <hongbin.lu@huawei.com>
2017-02-14 14:07:49 -0600

    Add tests for custom object fields
    Change-Id: Ie2534fa1297f5260257bd5fc063af0629115a51f

---
+++ zun/tests/unit/objects/test_fields.py
@@ -0,0 +1,60 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_versionedobjects.tests import test_fields
+
+from zun.objects import fields
+
+
+class TestContainerStatus(test_fields.TestField):
+    def setUp(self):
+        super(TestContainerStatus, self).setUp()
+        self.field = fields.ContainerStatus()
+        self.coerce_good_values = [
+            ('Error', 'Error'),
+            ('Running', 'Running'),
+            ('Stopped', 'Stopped'),
+            ('Paused', 'Paused'),
+            ('Unknown', 'Unknown'),
+            ('Creating', 'Creating'),
+        ]
+        self.coerce_bad_values = ['bad_value']
+        self.to_primitive_values = self.coerce_good_values[0:1]
+        self.from_primitive_values = self.coerce_good_values[0:1]
+
+    def test_stringify(self):
+        self.assertEqual("'Error'",
+                         self.field.stringify('Error'))
+
+    def test_stringify_invalid(self):
+        self.assertRaises(ValueError, self.field.stringify, 'bad_value')
+
+
+class TestTaskState(test_fields.TestField):
+    def setUp(self):
+        super(TestTaskState, self).setUp()
+        self.field = fields.TaskState()
+        self.coerce_good_values = [
+            ('image_pulling', 'image_pulling'),
+            ('container_creating', 'container_creating'),
+            ('sandbox_creating', 'sandbox_creating'),
+        ]
+        self.coerce_bad_values = ['bad_value']
+        self.to_primitive_values = self.coerce_good_values[0:1]
+        self.from_primitive_values = self.coerce_good_values[0:1]
+
+    def test_stringify(self):
+        self.assertEqual("'image_pulling'",
+                         self.field.stringify('image_pulling'))
+
+    def test_stringify_invalid(self):
+        self.assertRaises(ValueError, self.field.stringify, 'bad_value')
