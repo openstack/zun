83a693654c819ea107d8a0ab5fd4e4e096a32961
Ubuntu <ubuntu@devstack>
2017-08-04 08:04:27 +0000

    Update version of docker API
    To support running multiple runtimes in Zun, it is necessary to
    upgrade docker remote API version to 1.26 which supports passing
    ``runtime`` as input to create and run APIs.
    
    Change-Id: I3a3169fac3344e96068fdf6bd3baf72990e07ffc
    Partial-Implements: blueprint support-secure-container

--- zun/conf/docker.py
+++ zun/conf/docker.py
@@ -20,7 +20,7 @@ docker_group = cfg.OptGroup(name='docker',
 
 docker_opts = [
     cfg.StrOpt('docker_remote_api_version',
-               default='1.25',
+               default='1.26',
                help='Docker remote api version. Override it according to '
                     'specific docker api version in your environment.'),
     cfg.IntOpt('default_timeout',
