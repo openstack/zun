a3f78f4d61e18cefdde6fce6e482e0455374a6ac
Feng Shengqin <feng.shengqin@zte.com.cn>
2018-03-06 19:54:46 +0800

    Add disk filter
    Part of blueprint limit-container-diskspace
    
    Change-Id: I8b5a792181d4f4f603869ba6db76ff63561ed9f2

---
+++ zun/scheduler/filters/disk_filter.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2017 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+
+from zun.scheduler import filters
+
+LOG = logging.getLogger(__name__)
+
+
+class DiskFilter(filters.BaseHostFilter):
+    """Filter the containers by disk request"""
+
+    run_filter_once_per_request = True
+
+    def host_passes(self, host_state, container, extra_spec):
+        if not container.disk:
+            return True
+
+        usable_disk = host_state.disk_total - host_state.disk_used
+        if usable_disk < container.disk:
+            LOG.debug("%(host_state)s does not have %(container_disk)d "
+                      "usable disk, it only has %(usable_disk)d usable "
+                      "disk.",
+                      {'host_state': host_state,
+                       'container_disk': container.disk,
+                       'usable_disk': usable_disk})
+            return False
+        host_state.limits['disk'] = host_state.disk_total
+        return True
