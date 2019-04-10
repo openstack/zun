7c59ee9cb9ee90092f473514502cfb502d7a82db
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:18 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/271c7f45982d_add_started_at_to_container.py

--- zun/db/sqlalchemy/alembic/versions/271c7f45982d_add_started_at_to_container.py
+++ zun/db/sqlalchemy/alembic/versions/271c7f45982d_add_started_at_to_container.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add started_at to containers
 
 Revision ID: 271c7f45982d
@@ -24,8 +26,6 @@ down_revision = 'cff60402dd86'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
