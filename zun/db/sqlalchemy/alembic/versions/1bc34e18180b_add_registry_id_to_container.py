62b6cfe09c202075a43ca73dcff54cefd6a6e0a8
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:57:22 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/1bc34e18180b_add_registry_id_to_container.py

--- zun/db/sqlalchemy/alembic/versions/1bc34e18180b_add_registry_id_to_container.py
+++ zun/db/sqlalchemy/alembic/versions/1bc34e18180b_add_registry_id_to_container.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add registry_id to container
 
 Revision ID: 1bc34e18180b
@@ -24,8 +26,6 @@ down_revision = '5ffc1cabe6b4'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
