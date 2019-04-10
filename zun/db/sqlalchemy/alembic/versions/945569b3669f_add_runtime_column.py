ecc4c73eed2e10c06585b52720ae584cc6bafa57
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:57:14 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/945569b3669f_add_runtime_column.py

--- zun/db/sqlalchemy/alembic/versions/945569b3669f_add_runtime_column.py
+++ zun/db/sqlalchemy/alembic/versions/945569b3669f_add_runtime_column.py
@@ -10,6 +10,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add runtime column
 
 Revision ID: 945569b3669f
@@ -24,8 +26,6 @@ down_revision = 'a251f1f61217'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
