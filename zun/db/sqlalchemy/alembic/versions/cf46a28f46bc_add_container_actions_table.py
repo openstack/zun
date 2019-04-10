dde2c15532eef8d4b620a61dc4b403798827e164
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:24 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/cf46a28f46bc_add_container_actions_table.py

--- zun/db/sqlalchemy/alembic/versions/cf46a28f46bc_add_container_actions_table.py
+++ zun/db/sqlalchemy/alembic/versions/cf46a28f46bc_add_container_actions_table.py
@@ -10,6 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add container_actions table
 
 Revision ID: cf46a28f46bc
@@ -24,8 +26,6 @@ down_revision = 'd2affd5b4172'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
