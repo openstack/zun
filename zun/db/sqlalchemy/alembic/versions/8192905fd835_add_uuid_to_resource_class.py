c6080735dd28a52c757972f62d864a3d422a85a3
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:57:32 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/8192905fd835_add_uuid_to_resource_class.py

--- zun/db/sqlalchemy/alembic/versions/8192905fd835_add_uuid_to_resource_class.py
+++ zun/db/sqlalchemy/alembic/versions/8192905fd835_add_uuid_to_resource_class.py
@@ -10,6 +10,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add uuid_to_resource_class
 
 Revision ID: 8192905fd835
@@ -24,8 +26,6 @@ down_revision = 'e4d145e195f4'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
