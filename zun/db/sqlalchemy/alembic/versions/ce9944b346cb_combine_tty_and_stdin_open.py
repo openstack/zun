3a764728088f7ba28cd1e30bca16e79f2a546775
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:55:43 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/ce9944b346cb_combine_tty_and_stdin_open.py

--- zun/db/sqlalchemy/alembic/versions/ce9944b346cb_combine_tty_and_stdin_open.py
+++ zun/db/sqlalchemy/alembic/versions/ce9944b346cb_combine_tty_and_stdin_open.py
@@ -10,6 +10,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """combine tty and stdin_open
 
 Revision ID: ce9944b346cb
@@ -24,8 +26,6 @@ down_revision = '4bf34495d060'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
