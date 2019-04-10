c3e458fdb793077089f56284d907a9e7e1619d10
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:14 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/63a08e32cc43_add_task_state_to_container.py

--- zun/db/sqlalchemy/alembic/versions/63a08e32cc43_add_task_state_to_container.py
+++ zun/db/sqlalchemy/alembic/versions/63a08e32cc43_add_task_state_to_container.py
@@ -11,6 +11,8 @@
 #    under the License.
 
 
+from alembic import op
+import sqlalchemy as sa
 """add task state to container
 
 Revision ID: 63a08e32cc43
@@ -25,8 +27,6 @@ down_revision = '93fbb05b77b9'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
