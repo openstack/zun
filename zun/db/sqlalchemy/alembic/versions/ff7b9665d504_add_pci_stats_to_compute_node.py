05f4df9e04cd6d83f9552ec5971e585738d16887
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:47 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/ff7b9665d504_add_pci_stats_to_compute_node.py

--- zun/db/sqlalchemy/alembic/versions/ff7b9665d504_add_pci_stats_to_compute_node.py
+++ zun/db/sqlalchemy/alembic/versions/ff7b9665d504_add_pci_stats_to_compute_node.py
@@ -10,6 +10,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add pci stats to compute node
 
 Revision ID: ff7b9665d504
@@ -24,8 +26,6 @@ down_revision = 'fc27c7415d9c'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
