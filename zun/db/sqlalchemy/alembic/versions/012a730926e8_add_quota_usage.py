ca9a85dc7b7cebab773975c4e32b8b73d550c060
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:06 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/012a730926e8_add_quota_usage.py

--- zun/db/sqlalchemy/alembic/versions/012a730926e8_add_quota_usage.py
+++ zun/db/sqlalchemy/alembic/versions/012a730926e8_add_quota_usage.py
@@ -11,6 +11,8 @@
 #    under the License.
 
 
+from alembic import op
+import sqlalchemy as sa
 """Add quota usage
 
 Revision ID: 012a730926e8
@@ -25,8 +27,6 @@ down_revision = '3298c6a5c3d9'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
