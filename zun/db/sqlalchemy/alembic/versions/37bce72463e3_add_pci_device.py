9452f4a5ab2c9588beef7d23b1f9909a3f3ae2fd
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:38 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/37bce72463e3_add_pci_device.py

--- zun/db/sqlalchemy/alembic/versions/37bce72463e3_add_pci_device.py
+++ zun/db/sqlalchemy/alembic/versions/37bce72463e3_add_pci_device.py
@@ -10,6 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add pci device
 
 Revision ID: 37bce72463e3
@@ -24,8 +26,6 @@ down_revision = '10d65e285a59'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
