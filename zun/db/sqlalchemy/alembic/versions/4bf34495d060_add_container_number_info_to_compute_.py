6944b152bd205b701b1925216864d3c5df63b402
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:55:34 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/4bf34495d060_add_container_number_info_to_compute_.py

--- zun/db/sqlalchemy/alembic/versions/4bf34495d060_add_container_number_info_to_compute_.py
+++ zun/db/sqlalchemy/alembic/versions/4bf34495d060_add_container_number_info_to_compute_.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """Add container number info to compute node
 
 Revision ID: 4bf34495d060
@@ -24,8 +26,6 @@ down_revision = '53a8b515057e'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
