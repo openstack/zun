3b7b04918b4a5db0745b7c0f5ba96092af59dc9b
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:57 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/105626c4f972_add_privileged_to_container.py

--- zun/db/sqlalchemy/alembic/versions/105626c4f972_add_privileged_to_container.py
+++ zun/db/sqlalchemy/alembic/versions/105626c4f972_add_privileged_to_container.py
@@ -11,6 +11,8 @@
 #    under the License.
 
 
+from alembic import op
+import sqlalchemy as sa
 """add privileged to container
 
 Revision ID: 105626c4f972
@@ -25,8 +27,6 @@ down_revision = '3e80bbfd8da7'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
