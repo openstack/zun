dbd4217bb1308bb932aa0327159cf04f8a9b7c3b
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:04 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/a019998b09b5_add_host_to_image.py

--- zun/db/sqlalchemy/alembic/versions/a019998b09b5_add_host_to_image.py
+++ zun/db/sqlalchemy/alembic/versions/a019998b09b5_add_host_to_image.py
@@ -10,6 +10,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add host to image
 
 Revision ID: a019998b09b5
@@ -24,8 +26,6 @@ down_revision = 'a9c9fb54274a'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
