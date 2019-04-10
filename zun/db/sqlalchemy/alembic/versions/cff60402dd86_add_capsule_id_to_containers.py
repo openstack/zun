41fe41accf7e19105a68ababcab16861f3942f36
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:56:03 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/cff60402dd86_add_capsule_id_to_containers.py

--- zun/db/sqlalchemy/alembic/versions/cff60402dd86_add_capsule_id_to_containers.py
+++ zun/db/sqlalchemy/alembic/versions/cff60402dd86_add_capsule_id_to_containers.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """add capsule_id to containers
 
 Revision ID: cff60402dd86
@@ -24,8 +26,6 @@ down_revision = '2b045cb595db'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
