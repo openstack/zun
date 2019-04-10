7d2055aac7071fa1d4e54d215053217f4227bf16
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-09 19:57:54 -0700

    Fixed PYTHON_E402 issues in zun/db/sqlalchemy/alembic/versions/75315e219cfb_add_auto_remove_to_container.py

--- zun/db/sqlalchemy/alembic/versions/75315e219cfb_add_auto_remove_to_container.py
+++ zun/db/sqlalchemy/alembic/versions/75315e219cfb_add_auto_remove_to_container.py
@@ -10,6 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from alembic import op
+import sqlalchemy as sa
 """Add auto_remove to container
 
 Revision ID: 75315e219cfb
@@ -24,8 +26,6 @@ down_revision = '648c25faa0be'
 branch_labels = None
 depends_on = None
 
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
