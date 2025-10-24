5d3085c93ec0d6621c80f27535e39a58cd14a3a9
Michael Sherman <shermanm@uchicago.edu>
2025-10-24 13:35:28 -0500

    add basic tests for create

--- zun/tests/unit/container/kubernetes/test_k8s_driver.py
+++ zun/tests/unit/container/kubernetes/test_k8s_driver.py
@@ -1,19 +1,19 @@
 from unittest import mock
 
 from zun.container.k8s.driver import K8sDriver, zun_context
+from zun.container.k8s.driver import config as k8s_config
 from zun.container.k8s.network import K8sNetwork as zun_k8s_network
+from zun.objects.container import Container as ZunContainer
 from zun.tests.unit.container import base
-from zun.container.k8s.driver import (
-    client as k8s_client,
-    config as k8s_config,
-    watch as k8s_watch,
-)
+
+FAKE_PROJECT_ID = "aaaa-bbb-ccc-ddd"
 
 
 class TestK8sDriver(base.DriverTestCase):
     def setUp(self):
         super().setUp()
 
+        # initialize/mock required config options so the driver can start...
         self.mock_admin_context = mock.patch.object(
             zun_context, "get_admin_context"
         ).start()
@@ -23,10 +23,73 @@ class TestK8sDriver(base.DriverTestCase):
         self.driver = K8sDriver()
         self.mock_k8s = mock.MagicMock()
 
-        # initialize/mock required config options
+        # mock calls that create may make to the k8s client
+        self.mock_get_secrets_for_image = mock.patch.object(
+            self.driver, "_get_secrets_for_image"
+        ).start()
+
+        self.mock_create_namespaced_deployment = mock.patch.object(
+            self.driver.apps_v1, "create_namespaced_deployment"
+        ).start()
+
+        self.mock_create_namespaced_network_policy = mock.patch.object(
+            self.driver.net_v1, "create_namespaced_network_policy"
+        ).start()
 
     def test_create(self):
-        pass
+        """Test container create method.
+
+        This methid implements creating a zun container object, backed by a k8s deployment.
+        Returns the zun container object.
+        """
+
+        mock_image = mock.MagicMock()
+
+        mock_container = mock.MagicMock(
+            spec_set=ZunContainer,
+            project_id=FAKE_PROJECT_ID,
+            exposed_ports=[],
+        )
+
+        result_container = self.driver.create(
+            context=self.context,
+            container=mock_container,
+            image=mock_image,
+        )
+
+        # some basic assertions, ensure the project ID gets passed all the way through the stack of calls
+
+        self.assertEqual(result_container.project_id, FAKE_PROJECT_ID)
+
+        self.mock_get_secrets_for_image.assert_called_once()
+        self.mock_create_namespaced_deployment.assert_called_once()
+        self.assertEqual(
+            self.mock_create_namespaced_deployment.call_args[0][0], FAKE_PROJECT_ID
+        )
+
+        self.mock_create_namespaced_network_policy.assert_not_called()
+
+    def test_create_exposedports(self):
+        mock_image = mock.MagicMock()
+
+        mock_container = mock.MagicMock(
+            spec_set=ZunContainer,
+            project_id=FAKE_PROJECT_ID,
+            exposed_ports=["8000/tcp"],
+        )
+
+        result_container = self.driver.create(
+            context=self.context,
+            container=mock_container,
+            image=mock_image,
+        )
+        self.mock_create_namespaced_network_policy.assert_called_once()
+        self.assertEqual(
+            self.mock_create_namespaced_network_policy.call_args[0][0], FAKE_PROJECT_ID
+        )
+        called_spec = self.mock_create_namespaced_network_policy.call_args[0][1]
+        ingress_spec = called_spec.get("spec", {}).get("ingress", [])
+        self.assertEqual(ingress_spec[0]["ports"], [{"port": 8000, "protocol": "TCP"}])
 
     def test_commit(self):
         mock_container = mock.MagicMock()
